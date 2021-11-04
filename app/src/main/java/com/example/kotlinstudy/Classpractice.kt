de21f588ac559e0eaa2137f4c015911891d24f1b
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-04 12:47:09 +0900

    -kotlin Study. -class created

---
+++ app/src/main/java/com/example/kotlinstudy/Classpractice.kt
@@ -0,0 +1,15 @@
+package com.example.kotlinstudy
+
+
+class Human{
+    fun eatingcake(){
+        println("this is so yummy!!")
+    }
+}
+
+
+
+fun main(){
+    val human = Human()
+    human.eatingcake()
+}

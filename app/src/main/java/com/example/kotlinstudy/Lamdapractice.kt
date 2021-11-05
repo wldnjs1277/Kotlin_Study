c494d35beee7ab4367ce05c01f6d23252bebc657
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-05 18:30:06 +0900

    -Lamda

--- app/src/main/java/com/example/kotlinstudy/Lamdapractice.kt
+++ app/src/main/java/com/example/kotlinstudy/Lamdapractice.kt
@@ -8,4 +8,16 @@ val nameAge = {name : String, age : Int -> "my name is ${name} I'm ${age}"}
 fun main(){
     println(square(12))
     println(nameAge("jiwon",26))
+
+    val a = "jiwon said "
+    val b = "you said "
+    println(a.pizzaIsGreat())
+    println(b.pizzaIsGreat())
+
+}
+
+//확장함수
+
+val pizzaIsGreat : String.() -> String = {
+    this + "Pizza is the best !"
 }

21cd29e52dec1fe06984f5aa3d750ba0052eded8
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-05 18:21:39 +0900

    -Lamda

--- app/src/main/java/com/example/kotlinstudy/Lamdapractice.kt
+++ app/src/main/java/com/example/kotlinstudy/Lamdapractice.kt
@@ -1,9 +1,11 @@
 package com.example.kotlinstudy
 
 
-val square : (Int) -> (Int) = {number -> number*number}
-
+//val square : (Int) -> (Int) = {number -> number*number}
+val square  = {number : Int -> number*number}
+val nameAge = {name : String, age : Int -> "my name is ${name} I'm ${age}"}
 
 fun main(){
     println(square(12))
+    println(nameAge("jiwon",26))
 }

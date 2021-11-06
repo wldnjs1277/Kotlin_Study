aff491663a5ddf3100e6328cbae37ec949b7042b
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-06 19:00:40 +0900

    -Lamda practice

--- app/src/main/java/com/example/kotlinstudy/Lamdapractice1.kt
+++ app/src/main/java/com/example/kotlinstudy/Lamdapractice1.kt
@@ -6,7 +6,13 @@ val square1 = {number : Int -> number*number}
 val mynameAge = {name:String,age:Int -> "My name is ${name}, ${age} years old"}
 
 
+
 fun main(){
     println(square1(12))
     println(mynameAge("jiwon",26))
+    println(pizza("taste good "))
 }
+
+////////////
+
+val pizza : String.() -> String = {this + "pizza"}

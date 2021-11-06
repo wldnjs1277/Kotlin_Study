225ccbb8dcd82c23425ffee464858c1ef8dfcf21
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-06 19:11:41 +0900

    -Lamda practice

--- app/src/main/java/com/example/kotlinstudy/Lamdapractice1.kt
+++ app/src/main/java/com/example/kotlinstudy/Lamdapractice1.kt
@@ -13,8 +13,28 @@ fun main(){
     println(pizza("taste good "))
     val a = "jiwon said "
     println(a.pizza()+" taste good")
+
+    println(extendString1(name = "jiwon",age = 26))
+    println(Calculator(999))
 }
 
-////////////
+//////확장함수//////
 
 val pizza : String.() -> String = {this + "pizza"}
+
+fun extendString1(name:String,age:Int) : String{
+    val introduceMyself : String.(Int) -> String = {"I am ${this} and ${it}"}
+    return name.introduceMyself(age)
+}
+
+//람다의 Return
+
+val Calculator : (Int) -> String = {
+    when(it){
+        in 0..50 -> "fail"
+        in 51..70 -> "good"
+        in 71..100 -> "perfect"
+        else -> "error"
+    }
+
+}

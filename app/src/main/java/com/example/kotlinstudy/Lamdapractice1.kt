b34d08d4256d0f1e6d1a5f78adbafefdfd714059
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-06 18:41:46 +0900

    -Lamda practice

--- app/src/main/java/com/example/kotlinstudy/Lamdapractice1.kt
+++ app/src/main/java/com/example/kotlinstudy/Lamdapractice1.kt
@@ -3,8 +3,10 @@ package com.example.kotlinstudy
 
 val square1 = {number : Int -> number*number}
 
+val mynameAge = {name:String,age:Int -> "My name is ${name}, ${age} years old"}
+
 
 fun main(){
     println(square1(12))
-
+    println(mynameAge("jiwon",26))
 }

988bc7a2f99dd8023d0a5f4e6efe094a3fd1b468
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-06 20:08:17 +0900

    -Lamda practice

--- app/src/main/java/com/example/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlinstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

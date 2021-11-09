91026731b4fab2820201ca352c099e251a9bcceb
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-09 15:15:51 +0900

    -dataclass

--- app/src/main/java/com/example/kotlinstudy/DataClasspractice.kt
+++ app/src/main/java/com/example/kotlinstudy/DataClasspractice.kt
@@ -13,10 +13,10 @@ fun main(){
 
     val ticketB = Ticketnormal("koreanAir", "jiwon", "20201-11-8", 13)
 
-    var a = Ticket("test","a","11-8",1)
-    println(a.toString())
-    var b = a.copy()
-    println(b)
-   // println(ticketA)
-   // println(ticketB)
+    //var a = Ticket("test","a","11-8",1)
+    //println(a.toString())
+    //var b = a.copy()
+    //println(b)
+    println(ticketA)
+    println(ticketB)
 }

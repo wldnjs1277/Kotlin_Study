c6d4a3a9fe65d0ed55e7d4c87296f418ab00dbc5
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-09 14:01:09 +0900

    -dataclass

--- app/src/main/java/com/example/kotlinstudy/DataClasspractice.kt
+++ app/src/main/java/com/example/kotlinstudy/DataClasspractice.kt
@@ -5,11 +5,9 @@ data class Ticket(val CompanyName : String,val name : String,var date : String, 
 //toString(),hashCode(),equals(),copy()
 class Ticketnormal(val CompanyName : String,val name : String,var date : String, var seatNumber : Int)
 fun main(){
-    val ticketA = Ticket(CompanyName = "koreanAIR",name = "jiwon",
-        date = "20201-11-8",seatNumber = 13)
+    val ticketA = Ticket("koreanAir", "jiwon", "20201-11-8", 13)
 
-    val ticketB = Ticketnormal(CompanyName = "koreanAIR",name = "jiwon",
-        date = "20201-11-8",seatNumber = 13)
+    val ticketB = Ticketnormal("koreanAir", "jiwon", "20201-11-8", 13)
 
     println(ticketA)
     println(ticketB)

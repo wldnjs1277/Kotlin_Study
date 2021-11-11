0442270c250298796cb58867a3c0b05359ceacfd
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-11 15:14:21 +0900

    -object

---
+++ app/src/main/java/com/example/kotlinstudy/Ovjectpractice.kt
@@ -0,0 +1,21 @@
+package com.example.kotlinstudy
+
+fun main(){
+    val car1 = CarFactory.makeCar(10)
+    val car2 = CarFactory.makeCar(200)
+
+    println(car1)
+    println(car2)
+    println(CarFactory.cars.size.toString())
+}
+
+object  CarFactory {
+    val cars = mutableListOf<Car>()
+    fun makeCar(horsePower: Int) : Car{
+        val car = Car(horsePower)
+        cars.add(car)
+        return car
+    }
+}
+
+data class Car(val horsePower : Int)

3c99f1696ba08f3327dd054d639e03b215f17497
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-13 19:13:44 +0900

    -lamda/companionobject

--- app/src/main/java/com/example/kotlinstudy/Sample2.kt
+++ app/src/main/java/com/example/kotlinstudy/Sample2.kt
@@ -1,11 +1,14 @@
 package com.example.kotlinstudy
+
 //lamda/ companionobject
 
 fun main(){
     println(calculator(10,2))
 
-    val Jiwon = "jiwon said"
-    println(Jiwon.burger())
+    val jiwonfood ="burger"
+    println(jiwonfood.burger())
+
+    println(mymy("jiwon",26, "2021-11-13"))
 }
 
 //lamda
@@ -13,9 +16,17 @@ fun main(){
 val calculator = {number1:Int,number2:Int -> number1/number2}
 
 val burger : String.() -> String = {
-    this +" "+"burger good"
+    this+" "+"taste good"
+}
+
+fun mymy(name:String,age:Int,date:String) : String {
+    val introduce:String.(Int,String) -> String = {age:Int,date:String ->
+        "my name ${this} age = ${age} today = ${date}"
+    }
+    return name.introduce(age,date)
 }
 
 
 
 //companionobject
+

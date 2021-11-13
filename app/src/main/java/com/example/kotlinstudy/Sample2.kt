4ac8a14f79063b15cbf5aa77774ae5a91c8c880d
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-13 17:54:25 +0900

    -lamda/companionobject

--- app/src/main/java/com/example/kotlinstudy/Sample2.kt
+++ app/src/main/java/com/example/kotlinstudy/Sample2.kt
@@ -4,11 +4,17 @@ package com.example.kotlinstudy
 fun main(){
     println(calculator(10,2))
 
+    val Jiwon = "jiwon said"
+    println(Jiwon.burger())
 }
 
 //lamda
 
 val calculator = {number1:Int,number2:Int -> number1/number2}
+
+val burger : String.() -> String = {
+    this +" "+"burger good"
+}
 
 
 

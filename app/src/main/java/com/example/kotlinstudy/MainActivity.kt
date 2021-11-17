599b858479c8030a53f026d2518e82d137e31971
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-17 15:36:09 +0900

    -setOnClickListener

--- app/src/main/java/com/example/kotlinstudy/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudy/MainActivity.kt
@@ -8,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btn : Button = findViewById<Button>(R.id.button6)
+
+        btn.setOnClickListener {
+            btn.text = "버튼텍스트바꾸기"
+        }
     }
-}
+
+
+
+    }
